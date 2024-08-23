fb5b1b5d72dde3be8015c7fac1aa51531b6ae104
priyanka_bagul <tablabstechnologies@gmail.com>
2024-08-23 19:36:04 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -15,15 +15,15 @@ import './_metronic/assets/keenicons/solid/style.css'
 
 import './_metronic/assets/sass/style.scss'
 import { AppRoutes } from './app/routing/AppRoutes.tsx'
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/firebase-messaging-sw.js')
-    .then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch((error) => {
-      console.error('Service Worker registration failed:', error);
-    });
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/firebase-messaging-sw.js')
+//     .then((registration) => {
+//       console.log('Service Worker registered with scope:', registration.scope);
+//     })
+//     .catch((error) => {
+//       console.error('Service Worker registration failed:', error);
+//     });
+// }
 
 const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
